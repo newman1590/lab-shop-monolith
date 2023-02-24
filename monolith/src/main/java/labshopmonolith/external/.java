c9e1acc6e0f472faf7814543ee492925e349afa3
newman1590 <126225518+newman1590@users.noreply.github.com>
2023-02-24 17:03:57 +0900

    commit

---
+++ monolith/src/main/java/labshopmonolith/external/.java
@@ -0,0 +1,10 @@
+package labshopmonolith.external;
+
+import lombok.Data;
+import java.util.Date;
+@Data
+public class  {
+
+}
+
+
---
+++ monolith/src/main/java/labshopmonolith/external/Service.java
@@ -0,0 +1,16 @@
+package labshopmonolith.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+
+@FeignClient(name = "inventory", url = "${api.url.inventory}")
+public interface Service {
+    @RequestMapping(method= RequestMethod.PUT, path="/undefined/{id}/decreasestock")
+    public void decreaseStock(@PathVariable("id")  , @RequestBody DecreaseStockCommand decreaseStockCommand );
+}
